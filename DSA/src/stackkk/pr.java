04196db8ea8be3446b43ee40413bbc2a48330dbc
ManjunathKesarur <manjunath.dev05@gmail.com>
2026-09-05 23:02:06 +0530

    pr java

---
+++ DSA/src/stackkk/pr.java
@@ -0,0 +1,17 @@
+package stackkk;
+
+public class pr {
+
+	public  int  sum(int a) {
+	int c=a+a;
+	return c;	
+	}
+	
+	
+	public static void main(String[] args) {
+	
+		
+	System.out.println(	new pr().sum(12));
+	
+}
+}
