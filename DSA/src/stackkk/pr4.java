ccb38f858b6a79daaa4b18216d7673f057d11cd5
ManjunathKesarur <manjunath.dev05@gmail.com>
2026-09-06 12:27:20 +0530

    add pr4 in dsa

---
+++ DSA/src/stackkk/pr4.java
@@ -0,0 +1,18 @@
+package stackkk;
+
+public class pr4 {
+	public static void main(String[] args) {
+		
+	String day="tuesda";
+	
+	switch (day) {
+	
+	case "monday" : System.out.println("hello");
+	break;
+	
+	case "tuesday":System.out.println("tuesday");
+	break;
+	
+	default :System.out.println("lowda");
+	}
+	}}
